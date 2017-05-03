671bafd471113e4c424305abab3372b325a637dd
plakas20 <j.angelglez@hotmail.com>
2017-05-03 15:01:23 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const rq   = require('electron-require');
 const main = rq.remote('./main.js');
 const $    = require("jquery");
+//constantes para imprimir en pdf(ventana qe imprimimos)
+const ipc=require('electron').ipcRenderer
+const btnPDF.addEventListener('click',function(event){
+	ipc.send('print-to-pdf'))
 //http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=
 var buscarPersonaje = function(){
 	var personaje = $("#txtPersonaje").val();
@@ -14,9 +18,14 @@ var buscarPersonaje = function(){
 	}
 	url = url+personaje;
 	$.ajax({
+		beforeSend:function(){
+			$("#imgLoader").show();
+
+		}
 		dataType: "json",
 		url: url,
 		success:function(response){
+			$("#imgLoader").hide();
 			if(response.code==200){//ok.cat
 				$("#imgFoto").show("slow");
 				$("#imgFoto").attr("src",response.data.results[0].thumbnail.path+"."+
@@ -34,11 +43,24 @@ var buscarPersonaje = function(){
 				$("#txtComics").html(cadenaComics);
 			}			
 		}
+		$("#txtPersonaje").val(""); //vaciamos el cuadro de texto 
+		$("#txtPersonaje").focus(); //ponemos el cursor ahi
 	})
+
+}
+
+var teclaPersonaje=function(tecla){
+	//Enter=10,13(avance de linea y retorno de carro)
+if (tecla.wich==13) {
+buscarPersonaje();
+
+}
 
 }
 
 //Posiciona el cursor en el cuadro de texto
 $("#txtPersonaje").focus();
+//Activar las teclas qe se presionan en el cuadro de texto 
+$("#txtPersonaje").on("keypress",teclaPersonaje);
 //Evento del botón btnBuscar-click
 $("#btnBuscar").on("click",buscarPersonaje);
--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const {app,BrowserWindow} = require('electron')
+const electron=require
 //Ruta donde se encuentra nuestro proyecto
 const path = require('path')
 //Ruta pero en formato URL
 const url  = require('url')
+//constantes para impresion en PDF
+const fs= require('fs')//sistemas de archivos
+const os=require('os')//sistemas operativo
+//llamada o procedimiento interno
+const ipc=electron.ipcMain
+//interfaz de comandos o terminal
+const shell=electron.shell
+
+//evento para imprimir un PDF
+ipc.on('print-to-pdf', function(event){
+const pdfPath=path.join(os.tmpdir(), 'print.pdf')
+const win=BrowserWindow.fromWebContenst(event.sender)
+win.webContents.printToPDF({},function(error,data){
+	if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+			if (error) {
+				throw error
+			}
+		shell.openExternal('file://'+pdfPath)
+
+		})
+})
+})
 let PantallaPrincipal;
 function muestraPantallaPrincipal(){
 	PantallaPrincipal = new BrowserWindow({
