5387315becd4677f03558b9bf0cad06beb4ee331
plakas20 <j.angelglez@hotmail.com>
2017-05-08 14:58:27 -0600

    Add files via upload

---
+++ validausuario.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST["txtusuario"])&& isset($_POST["txtClave"])) {
+	
+
+$usuario=$_POST[""]
+$clave=$_POST[""]
+print($usuario);
+print($clave);
+
+}
+else{
+	print("<a href='acceso.html'>Valida tus datos</a>");
+}
+?>
