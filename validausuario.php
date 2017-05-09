8ff77c4cf1344ed9ffdd7c5662d86c49d2ba846e
plakas20 <j.angelglez@hotmail.com>
2017-05-09 15:02:03 -0600

    Add files via upload

--- validausuario.php
+++ validausuario.php
@@ -1,11 +1,26 @@
 <?php
+include("utilerias.php");
+function validausuario($usuario,$clave){
+//Computadora, usuario,contraseña
+	$conexion=msql_connect("localhost","root","");
+	msql_select_db("pw2171");
+	$usuario=GetSQLValueString($usuario,"text");
+	$clave=GetSQLValueString(md5($clave),"text");
+$consulta=sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s",$usuario,$clave);
+
+	//$consulta="select usuario,clave from usuarios where usuario='".$usuario"and clave='".md5($clave)."' limit 1";
+    $resultado=msql_query($consulta);
+if (msql_numrows($resultado)>0) {
+print("Bievenido".$usuario.":)");
+}else{
+	print("NO ERES BIENVENIDO");
+}
 if (isset($_POST["txtusuario"])&& isset($_POST["txtClave"])) {
 	
 
-$usuario=$_POST[""]
-$clave=$_POST[""]
-print($usuario);
-print($clave);
+$usuario=$_POST["txtusuario"];
+$clave=$_POST["txtClave"]
+validausuario($usuario,$clave);
 
 }
 else{
